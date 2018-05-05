2047aea8d689a7ba11dae0b0647d4cf6ee7b301b
Gab Paras <mgvparas@gmail.com>
2018-05-05 21:42:36 +0800

    renamed tile properties

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 class Tile {
-    constructor(x, y, index) {
-        this.x = x;
-        this.y = y;
-        this.index = index;
+    constructor(rowIndex, columnIndex, number) {
+        this.rowIndex = rowIndex;
+        this.columnIndex = columnIndex;
+        this.number = number;
     }
 }
 
@@ -19,12 +19,21 @@ class Board {
     }
 
     swapWithEmpty(tile) {
-        const emptyTile = this.tiles.find(x => x.index === 15);
+        const adjacentTiles = this._getAdjacentTiles(tile);
+        const emptyTile = this.tiles.find(x => x.number === 15);
 
-        emptyTile.index = tile.index;
-        tile.index = 15;
+        emptyTile.number = tile.number;
+        tile.number = 15;
         
         console.log(this.tiles);
+    }
+
+    _getAdjacentTiles(tile) {
+        let adjacentTiles = [];
+        if (tile.y )this.tiles.find(x => x.x === tile.x);
+
+
+        return adjacentTiles;
     }
 }
 
@@ -42,20 +51,23 @@ class BoardComponent {
             tileDiv.style.width = "50px";
             tileDiv.style.display = "inline-block";
             tileDiv.style.position = "absolute";
-            tileDiv.style.left = (tile.y * 55) + "px";
-            tileDiv.style.top = (tile.x * 55) + "px";
+            tileDiv.style.left = (tile.columnIndex * 55) + "px";
+            tileDiv.style.top = (tile.rowIndex * 55) + "px";
+            // tileDiv.style.transition = "0.15s top ease-in-out, 0.15s left ease-in-out";
             
-            if (tile.index === 15) {
+            if (tile.number === 15) {
                 tileDiv.style.backgroundColor = "white";
             } else {
                 tileDiv.style.backgroundColor = "blue";
-                tileDiv.appendChild(document.createTextNode(tile.index));
+                tileDiv.appendChild(document.createTextNode(tile.number));
             };
 
             tileDiv.addEventListener('click', () => {
-                console.log(`Tile ${tile.index} clicked!`);
+                console.log(`Tile ${tile.number} clicked!`);
 
                 this.board.swapWithEmpty(tile);
+
+                parentNode.innerHTML = "";
                 this.render(parentNode);
             });
 
