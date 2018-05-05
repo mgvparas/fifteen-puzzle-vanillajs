005d085fcdd31402d014c9926b1360a1371e615d
Gab Paras <mgvparas@gmail.com>
2018-05-05 13:18:16 +0800

    swap tile with empty one.

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ class Tile {
 class Board {
     constructor(rowCount = 4, columnCount = 4) {
         this.tiles = [];
-        this.emptyTile = {};
 
         let tileIndex = 0;
         for(let x = 0; x < rowCount; x++) {
             for(let y = 0; y < columnCount; y++) {
-                if (x === rowCount - 1 && y === columnCount - 1) {
-                    this.emptyTile = new Tile(x, y, tileIndex++);
-                } else {
-                    this.tiles.push(new Tile(x, y, tileIndex++));
-                }
+                this.tiles.push(new Tile(x, y, tileIndex++));
             }
         }
+    }
+
+    swapWithEmpty(tile) {
+        const emptyTile = this.tiles.find(x => x.index === 15);
+
+        emptyTile.index = tile.index;
+        tile.index = 15;
+        
+        console.log(this.tiles);
     }
 }
 
@@ -36,31 +40,27 @@ class BoardComponent {
             const tileDiv = document.createElement("div");
             tileDiv.style.height = "50px";
             tileDiv.style.width = "50px";
-            tileDiv.style.backgroundColor = "blue";
             tileDiv.style.display = "inline-block";
             tileDiv.style.position = "absolute";
             tileDiv.style.left = (tile.y * 55) + "px";
             tileDiv.style.top = (tile.x * 55) + "px";
             
-            tileDiv.appendChild(document.createTextNode(tile.index));
+            if (tile.index === 15) {
+                tileDiv.style.backgroundColor = "white";
+            } else {
+                tileDiv.style.backgroundColor = "blue";
+                tileDiv.appendChild(document.createTextNode(tile.index));
+            };
 
             tileDiv.addEventListener('click', () => {
                 console.log(`Tile ${tile.index} clicked!`);
+
+                this.board.swapWithEmpty(tile);
+                this.render(parentNode);
             });
 
             boardDiv.appendChild(tileDiv);
         }
-
-        const emptyTileDiv = document.createElement("div");
-        emptyTileDiv.style.height = "50px";
-        emptyTileDiv.style.width = "50px";
-        emptyTileDiv.style.background = "transparent";
-        emptyTileDiv.style.display = "inline-block";
-        emptyTileDiv.style.position = "absolute";
-        emptyTileDiv.style.left = (this.board.emptyTile.y * 55) + "px";
-        emptyTileDiv.style.top = (this.board.emptyTile.x * 55) + "px";
-
-        boardDiv.appendChild(emptyTileDiv);
 
         parentNode.appendChild(boardDiv);
     }
