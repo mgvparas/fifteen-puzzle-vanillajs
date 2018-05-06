64b68c659e9dd29eec713e7d0c0f498e9c202d27
Gab Paras <mgvparas@gmail.com>
2018-05-07 00:01:18 +0800

    returned board component class to its own file.

---
+++ src/Components/BoardComponent.js
@@ -0,0 +1,44 @@
+class BoardComponent {
+    constructor(board) {
+        this.board = board;
+    }
+
+    render(parentNode) {
+        const boardDiv = document.createElement('div');
+        
+        for(const tile of this.board.tiles) {
+            let text;
+            let styles = { height: 50, width: 50, backgroundColor: 'blue' };
+
+            if (tile.number === this.board.tileCount) {
+                styles.backgroundColor = 'white';
+            } else {
+                text = tile.number;
+            };
+
+            const tileComponent = new TileComponent(
+                tile,
+                text,
+                styles,
+                () => this._handleTileClick(tile, parentNode)
+            );
+
+            tileComponent.render(boardDiv);
+        }
+
+        parentNode.appendChild(boardDiv);
+    }
+    
+    _handleTileClick(tile, parentNode) {
+        console.log(`Tile ${tile.number} clicked!`);
+    
+        this.board.swapWithEmpty(tile)
+    
+        parentNode.innerHTML = "";
+        this.render(parentNode);
+    
+        if (this.board.isSolved) {
+            alert('Puzzle Solved! :)');
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,51 +1,6 @@
 const board = new Board();
 board.shuffle();
 
-class BoardComponent {
-    constructor(board) {
-        this.board = board;
-    }
-
-    render(parentNode) {
-        const boardDiv = document.createElement('div');
-        
-        for(const tile of this.board.tiles) {
-            let text;
-            let styles = { height: 50, width: 50, backgroundColor: 'blue' };
-
-            if (tile.number === this.board.tileCount) {
-                styles.backgroundColor = 'white';
-            } else {
-                text = tile.number;
-            };
-
-            const tileComponent = new TileComponent(
-                tile,
-                text,
-                styles,
-                () => this._handleTileClick(tile, parentNode)
-            );
-
-            tileComponent.render(boardDiv);
-        }
-
-        parentNode.appendChild(boardDiv);
-    }
-    
-    _handleTileClick(tile, parentNode) {
-        console.log(`Tile ${tile.number} clicked!`);
-    
-        this.board.swapWithEmpty(tile)
-    
-        parentNode.innerHTML = "";
-        this.render(parentNode);
-    
-        if (this.board.isSolved) {
-            alert('Puzzle Solved! :)');
-        }
-    }
-}
-
 const boardComponent = new BoardComponent(board);
 const main = document.getElementsByClassName("main")[0];
 boardComponent.render(main);
